de668a6a31c11c377b15016881b8a39547a22f25
Sajjaddi <sajjadjahanids@gmail.com>
2023-02-14 19:42:18 +0330

    configuration tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      xs: '360px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
+    extend: {
+      maxWidth: {
+        '8xl': '1500px'
+      },
+      colors: {
+        tara: {
+          primary: '#11011E',
+          secondary: '#0FAE96',
+          tertiary: '#AE0000',
+          quaternary: '#211631'
+        },
+      },
+    },
   },
   plugins: [],
 }
